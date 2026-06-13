99a6e3c103c2b27878ee978211df7fb3c34f0512
nfforbes <nfforbes@gmail.com>
2026-06-13 01:10:29 -0400

    update layout

--- app/layout.js
+++ app/layout.js
@@ -118,6 +118,18 @@ export default function RootLayout({ children }) {
             }),
           }}
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wx752zjhnl");
+            `
+          }}
+        />
       </head>
             <body suppressHydrationWarning={true}>
               <UserProvider>
